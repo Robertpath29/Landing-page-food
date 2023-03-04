40ca5cc3d5989d50622e4f4b8576b23d503806ca
Yevhenii <Robertpath29@gmail.com>
2023-03-04 12:08:47 +0200

    add section Best-seller

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import "./index.pug"
 import "./style.scss"
 import "/node_modules/normalize.css/normalize.css";
+
+
+
 import "./img/logo.png"
+import "./img/BestSeler-img-1.png"
+import "./img/BestSeler-img-2.png"
+import "./img/BestSeler-img-3.png"
+
+
+
 
 
 
